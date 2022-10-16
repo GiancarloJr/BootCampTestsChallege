26d67e7f4ec050e97b43780885c98d3e5a1d0d74
GiancarloJr <giancarlo456@hotmail.com>
2022-10-16 18:56:21 -0300

    Event Tests Integração Completo/ City Tests Refinado

--- backend/src/main/java/com/devsuperior/bds02/resource/EventResource.java
+++ backend/src/main/java/com/devsuperior/bds02/resource/EventResource.java
@@ -1,9 +1,30 @@
 package com.devsuperior.bds02.resource;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.services.EventService;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/events")
 public class EventResource {
+
+    private EventService eventService;
+
+    public EventResource(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventDTO> updateEvent(@PathVariable Long id, @RequestBody EventDTO eventDTO){
+        try{
+            eventDTO.setId(id);
+            return ResponseEntity.ok().body(eventService.update(eventDTO));
+        } catch (ResourceNotFoundException e){
+            return ResponseEntity.notFound().build();
+
+        }
+    }
 }
--- backend/src/main/java/com/devsuperior/bds02/services/EventService.java
+++ backend/src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -2,12 +2,14 @@ package com.devsuperior.bds02.services;
 
 import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repository.CityRepository;
 import com.devsuperior.bds02.repository.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class EventService {
@@ -15,24 +17,22 @@ public class EventService {
     @Autowired
     private EventRepository eventRepository;
 
-    public List<EventDTO> findAll(){
-        List<Event> entities = eventRepository.findAll();
-        List<EventDTO> dtos = new ArrayList<>();
-        EventDTO dto = new EventDTO();
-        for(Event entity: entities){
-            dto.setId(entity.getId());
-            dto.setName(entity.getName());
-            dto.setDate(entity.getDate());
-            dto.setUrl(entity.getUrl());
-            dto.setCityId(entity.getCity().getId());
-            dtos.add(dto);
+    @Autowired
+    private CityRepository cityRepository;
+
+    public void convertDtoToEntity(Event entity,EventDTO dto){
+        entity.setCity(cityRepository.getOne(dto.getCityId()));
+        entity.setDate(dto.getDate());
+        entity.setName(dto.getName());
+        entity.setUrl(dto.getUrl());
+    }
+    public EventDTO update(EventDTO eventDTO) {
+        try {
+            Optional<Event> obj = eventRepository.findById(eventDTO.getId());
+            convertDtoToEntity(obj.get(),eventDTO);
+            return new EventDTO(eventRepository.save(obj.get()));
+        } catch (NoSuchElementException e){
+            throw new ResourceNotFoundException("Entity not found");
         }
-        return dtos;
     }
-
-    public EventDTO convertyEntityToDto(Event entity){
-        EventDTO dto = new EventDTO(entity.getId(),entity.getName(),entity.getDate(), entity.getUrl(), entity.getCity().getId());
-        return dto;
-    }
-
 }
